735fc2197c1cd751236288dcb1c43d6f9eb8d955
Joaquim <joaquimlima0612@gmail.com>
2025-07-04 14:52:02 -0300

    Update des027.py

--- desafios/des027.py
+++ desafios/des027.py
@@ -1,7 +1,6 @@
 # Faça um programa que leia o nome completo de uma pessoa mostrando em seguida o primeiro e o último nome separadamente. Ex.: Ana Maria de Souza (primeiro nome: Ana e último nome: Souza)
 
 nome = input('Escreva seu nome completo: ').strip()
-
 dividido = nome.split()
 
 print('Primeiro nome: {} \nÚltimo Nome: {}'.format(dividido[0], dividido[-1])) # -1 para pegar o último elemento da lista
