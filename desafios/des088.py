5aa074cca5c2f5ad88457c0fe4f7ee8f5d46b093
Joaquim <joaquimlima0612@gmail.com>
2025-07-13 12:17:39 -0300

    Finalização dos desafios

--- desafios/des088.py
+++ desafios/des088.py
@@ -1,4 +1,31 @@
 # Faça um programa que ajude um jogador da MEGA SENA a criar palpites. O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta
 
-import random
-computador = random.randint(1, 60)
+from random import randint
+from time import sleep
+lista = list()
+jogos = list()
+print('-'*30)
+print('     Programa MEGA SENA      ')
+print('-'*30)
+qtd = int(input('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+
+while tot <= qtd: # Enquanto o total de jogadas chegar nas digitadas...
+    cont = 0
+    while True:
+        numero = randint(1, 60)
+        if numero not in lista: # Se o número sorteado NÃO estiver na lista...
+            lista.append(numero)
+            cont += 1
+        if cont >= 6: # Se ja foram sorteados 6 números...
+            break
+    lista.sort()
+    jogos.append(lista[:]) # Criando uma lista grande (jogos) cheia de listas pequenas (lista)
+    lista.clear()
+    tot += 1 # Para não entrar em loop infinito
+print('-='*3, f'Sorteando {qtd} jogos', '-='*3)
+
+for indice, l in enumerate(jogos): # Para cada índice e lista...
+    print(f'Jogo {indice + 1}: {l}') # Mostra esse painelzinho
+    sleep(1) # Esperando 1 segundo a cada print
+print('-='*5, '< BOA SORTE! >', '-='*5)
--- desafios/des089.py
+++ desafios/des089.py
@@ -1,1 +1,28 @@
 # Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente
+
+boletim = list()
+
+while True:
+    nome = input('Digite o nome do aluno: ')
+    n1 = int(input('Digite a primeira nota dele: '))
+    n2 = int(input('Digite a segunda nota dele: '))
+    media = (n1 + n2) / 2
+    boletim.append([nome, [n1, n2], media])
+    escolha = input('Quer continuar?[S/N] ').strip().upper()[0]
+    if escolha == 'N':
+        break
+    elif escolha != 'S':
+        print('Inválido, assumimos que sim...')
+
+for indice, aluno in enumerate(boletim):
+    print(f'{indice:<4}{aluno[0]:<10}{aluno[2]:>8.1f}') # Imprime o número/posição do aluno (indice), o nome dele (aluno[0]) e a média dele (aluno[2])
+
+# Mostrando as notas individualmente
+while True:
+    opcao = int(input('\nDeseja mostrar as notas de qual aluno? (999 para interromper): '))
+    if opcao == 999:
+        break
+    if 0 <= opcao < len(boletim): # Só pode digitar um número entre 0 e o comprimento da lista (se tiver só dois elementos, 1)
+        print(f'As notas de {boletim[opcao][0]} são: {boletim[opcao][1]}') # boletim[opcao][0] = Nome; boletim[opcao][1] = notas; boletim[opcao][2] = média
+    else:
+        print('Número Inválido')
