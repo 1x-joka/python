fd0f19f368cb45b8d3cdd92af54da32e976bfed0
Joaquim <joaquimlima0612@gmail.com>
2025-07-14 17:02:26 -0300

    Realização dos desafios2

--- desafios/des092.py
+++ desafios/des092.py
@@ -1,1 +1,20 @@
 # Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário se por acaso a CTPS for diferente de zero, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar
+
+from datetime import datetime
+ano_atual = datetime.now().year
+
+dados = dict()
+
+dados['nome'] = input('Digite o nome: ')
+dados['anonasc'] = int(input('Digite o ano de nascimento: '))
+dados['idade'] = ano_atual - dados['anonasc']
+dados['carteira'] = int(input('Digite a carteira de trabalho(0 se não tiver): '))
+    
+if dados['carteira'] != 0:
+    dados['ano_contratacao'] = int(input('Ano de contratação: '))
+    dados['salario'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = (dados['ano_contratacao'] + 35) - dados['anonasc']
+
+print('\nDados Cadastrados:')
+for k, v in dados.items():
+    print(f'{k.capitalize()}: {v}')
--- desafios/des093.py
+++ desafios/des093.py
@@ -1,1 +1,22 @@
 # Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato
+
+dados = dict()
+
+dados['nome'] = input('Digite o nome do jogador: ').strip()
+totalpartidas = int(input(f'Quantas partidas {dados["nome"]} jogou: '))
+dados['partidas'] = totalpartidas
+
+gols = list()
+
+for indice in range(totalpartidas):
+    gol = int(input(f'Quantos gols ele fez na {indice + 1}° partida: '))
+    gols.append(gol) # Adicionando cada gol na lista
+
+dados['gols'] = gols # Passando a lista para o dicionário
+dados['total'] = sum(gols) # Somando os gols feitos
+
+print('\nResumo do jogador:')
+print(f'Nome: {dados["nome"]}')
+print(f'Número de partidas: {dados["partidas"]}')
+print(f'Gols por partida: {dados["gols"]}')
+print(f'Total de gols no campeonato: {dados["total"]}')
