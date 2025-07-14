f06d5c561290015bcff258d35ebceb1d6c8baa32
Joaquim <joaquimlima0612@gmail.com>
2025-07-14 17:40:41 -0300

    Finalização dos desafios

--- desafios/des094.py
+++ desafios/des094.py
@@ -1,1 +1,55 @@
 # Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre: a) Quantas pessoas foram cadastradas; b) A média de idade do grupo; c) Uma lista com todas as mulheres; d) Uma lista com todas as pessoas com idade acima da média
+
+from time import sleep
+
+pessoas = []
+soma_idade = 0
+
+print('-='*20)
+print(f'{"Programa Cálculo":^40}')
+print('-='*20)
+
+while True:
+    dados = dict() # Não pode ficar antes desse while pois eu estarei sobrescrevendo cada pessoa quando der loop
+    dados['nome'] = input('Digite seu nome: ').strip()
+    while True:
+        dados['sexo'] = input('Digite seu sexo: ').strip().upper()[0]
+
+        if dados['sexo'] in 'MF':
+            break
+        print('[ERRO] Digite apenas M ou F')
+
+    dados['idade'] = int(input('Digite sua idade: '))
+    soma_idade += dados['idade'] # Somando as idades digitadas
+    pessoas.append(dados.copy()) # Copiando todo o dicionário na lista
+
+    escolha = input('Quer continuar?[S/N]: ').strip().upper()[0]
+    if escolha == 'N':
+        break
+    elif escolha != 'S':
+        print('Inválido, assumindo que SIM...')
+
+print('-='*20)
+print(f'{"Calculando...":^40}')
+print('-='*20)
+sleep(1)
+
+print(f'Foram cadastradas {len(pessoas)} pessoas')
+media = soma_idade / len(pessoas)
+print(f'A média das idades é {media:.2f} anos')
+
+# Lista de Mulheres:
+mulheres = []
+for p in pessoas:
+    if p['sexo'] == 'F':
+        mulheres.append(p['nome']) # Guardando o nome das mulheres na lista 'mulheres'
+print(f'As mulheres da lista são {mulheres}')
+
+# Pessoas acima da média:
+print('Pessoas com idade acima da média:')
+for p in pessoas:
+    if p['idade'] > media:
+        print(f'Nome: {p["nome"]}, Sexo: {p["sexo"]}, Idade: {p["idade"]}')
+
+# ERROS:
+# lista.append(dados['idade'][:]) --> Não faz sentido pois INT é infatiável
--- desafios/des095.py
+++ desafios/des095.py
@@ -1,1 +1,45 @@
 # Aprimore o DESAFIO 093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador
+
+jogadores = []
+
+while True:
+    dados = dict()
+    gols = []
+    dados['nome'] = input('Digite o nome do jogador: ').strip()
+    totalpartidas = int(input(f'Quantas partidas {dados["nome"]} jogou: '))
+
+    for indice in range(totalpartidas):
+        gol = int(input(f'Quantos gols ele fez na {indice + 1}° partida: '))
+        gols.append(gol) # Adicionando cada gol na lista
+
+    dados['gols'] = gols # Passando a lista para o dicionário
+    dados['total'] = sum(gols) # Somando os gols feitos
+
+    jogadores.append(dados.copy())
+
+    escolha = input('Quer continuar?[S/N]: ').strip().upper()[0]
+    if escolha == 'N':
+        break
+    elif escolha != 'S':
+        print('Inválido, assumimos que SIM...')
+
+# Tabela com Resumo
+print('\n' + '-' * 40)
+print(f'{"cod":<4}{"nome":<15}{"gols":<15}{"total":<5}') # Imprime um cabeçalho formatado para a tabela de jogadores, alinhando os títulos das colunas de forma organizada
+print('-' * 40)
+
+for indice,jogador in enumerate(jogadores):
+    print(f'{indice:<4}{jogador["nome"]:<15}{str(jogador["gols"]):<15}{jogador["total"]:<5}') # Imprimindo uma linha formatada com 4 colunas
+print('-'*40)
+
+# Tabela de visualização individual
+while True:
+    opcao = int(input('Quer mostrar os dados de qual jogador?(999 para interromper): '))
+    if opcao == 999:
+        break
+    if 0 <= opcao < (len(jogadores)): # Verifica se a opção condiz com a lista jogadores
+        print(f'Detalhes do jogador {jogadores[opcao]["nome"]}: ') # Acessando o nome do jogador na estrutura de dados criado
+        for indice, gol in enumerate(jogadores[opcao]['gols']):
+            print(f'No jogo {indice + 1} fez {gol} gols')
+    else:
+        print('[ERRO] Código Inválido. Tente novamente')
